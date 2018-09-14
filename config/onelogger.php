4c9288781eb1ce5a03c515202c50576d63088cb2
mkornatz <mkornatz@hush.com>
2018-09-14 17:39:17 -0400

    Removes onelogger stuff

--- config/onelogger.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * This is optional. This is only used if the onedesign/onelogger plugin is installed.
- */
-return [
-  'logLevels' => getenv('ONELOGGER_LOG_LEVELS') ? explode(',', getenv('ONELOGGER_LOG_LEVELS')) : ['warning', 'error']
-];
