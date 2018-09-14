e564b08c57cb8e5d01cfe3729f3a0adf3fa64fe8
mkornatz <mkornatz@hush.com>
2018-09-14 17:37:35 -0400

    Removes heroku stuff since we won't be hosting there anymore

--- config/onelogger.php
+++ config/onelogger.php
@@ -1,5 +1,7 @@
 <?php
-
+/**
+ * This is optional. This is only used if the onedesign/onelogger plugin is installed.
+ */
 return [
   'logLevels' => getenv('ONELOGGER_LOG_LEVELS') ? explode(',', getenv('ONELOGGER_LOG_LEVELS')) : ['warning', 'error']
 ];
