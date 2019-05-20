21b4c97a94450211f88ed459b26a97323ada8e76
Michael McMillan <mikemcchillin@gmail.com>
2019-05-20 11:19:23 -0500

    Added conditional for cdn in assetrev

--- config/assetrev.php
+++ config/assetrev.php
@@ -4,6 +4,15 @@
  *
  * @link https://github.com/clubstudioltd/craft3-asset-rev#configuration
  */
+
+// This allows us to use a CDN for serving assets directly on the site
+// Set ASSET_REV_CDN_URL_PREFIX to something like https://268409-832646-raikfcquaxqncofqfm.stackpathdns.com
+if (getenv('ASSET_REV_CDN_URL_PREFIX')) {
+  $assetUrlPrefix = rtrim(getenv('ASSET_REV_CDN_URL_PREFIX'), '/') . '/dist/';
+} else {
+  $assetUrlPrefix = '/dist/';
+}
+
 return array(
   '*' => array(
     'strategies' => [
@@ -16,6 +25,6 @@ return array(
     'pipeline' => 'manifest|querystring|passthrough',
     'manifestPath' => 'web/dist/rev-manifest.json',
     'assetsBasePath' => 'web/dist/',
-    'assetUrlPrefix' => '/dist/'
+    'assetUrlPrefix' => $assetUrlPrefix
   )
 );
