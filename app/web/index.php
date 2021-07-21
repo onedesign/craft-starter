80f53771e1f1ff932e04227537c8cdeac3e38379
Brian Hanson <brian@brianhanson.net>
2021-07-21 11:25:04 -0500

    Sync with upstream

--- app/web/index.php
+++ app/web/index.php
@@ -3,8 +3,8 @@
  * Craft web bootstrap file
  */
 
-// Set path constants (uses absolute path to avoid caching issues with php-fpm and atomic deploys using symlinks)
-define('CRAFT_BASE_PATH', realpath(dirname(__DIR__)));
+// Define path constants
+define('CRAFT_BASE_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
 
 // Load Composer's autoloader
@@ -15,8 +15,12 @@ if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH . '/.env')) {
     Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
 }
 
-// Load and run Craft
+// Define additional PHP constants
+// (see https://craftcms.com/docs/3.x/config/#php-constants)
 define('CRAFT_ENVIRONMENT', getenv('ENVIRONMENT') ?: 'production');
+// ...
+
+// Load and run Craft
 /** @var craft\web\Application $app */
 $app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
