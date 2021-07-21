0ace411905f2e52c9e662367535230cdc0af7788
Brian Hanson <brian@brianhanson.net>
2021-07-21 10:55:29 -0500

    Import cleanup

--- app/config/assetrev.php
+++ app/config/assetrev.php
@@ -7,6 +7,10 @@
 
 // This allows us to use a CDN for serving assets directly on the site
 // Set ASSET_REV_CDN_URL_PREFIX to something like https://268409-832646-raikfcquaxqncofqfm.stackpathdns.com
+
+use club\assetrev\utilities\strategies\ManifestFileStrategy;
+use club\assetrev\utilities\strategies\QueryStringStrategy;
+
 if (getenv('ASSET_REV_CDN_URL_PREFIX')) {
   $assetUrlPrefix = rtrim(getenv('ASSET_REV_CDN_URL_PREFIX'), '/') . '/dist/';
 } else {
@@ -15,13 +19,13 @@ if (getenv('ASSET_REV_CDN_URL_PREFIX')) {
 
 return array(
   '*' => array(
-    'strategies' => [
-      'manifest' => \club\assetrev\utilities\strategies\ManifestFileStrategy::class,
-      'querystring' => \club\assetrev\utilities\strategies\QueryStringStrategy::class,
-      'passthrough' => function ($filename, $config) {
+    'strategies' => array(
+      'manifest' => ManifestFileStrategy::class,
+      'querystring' => QueryStringStrategy::class,
+      'passthrough' => function ($filename) {
           return $filename;
       },
-    ],
+    ),
     'pipeline' => 'manifest|querystring|passthrough',
     'manifestPath' => 'web/dist/rev-manifest.json',
     'assetsBasePath' => 'web/dist/',
